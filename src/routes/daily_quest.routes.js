ecc0d585e7f5010fcfe8593a5339f92b2ad7dfc4
Oriol Canellas <oriolcanellas3@gmail.com>
2026-06-18 09:50:09 +0200

    w3retfyg

--- src/routes/daily_quest.routes.js
+++ src/routes/daily_quest.routes.js
@@ -11,11 +11,14 @@ const {
     deleteDailyQuest
 } = require("../controllers/Daily_quest.CRUD.js");
 
-router.get("/reward/:reward", getReward);   // Filtrar por recompensa
+router.get("/reward", getReward);   // Filtrar por recompensa
 router.get("/all", getDailyQuests);               // Todos los quests completos
+router.get("/description", getDescription); // Filtrar por descripción
+
 router.get("/", getDailyQuests);                  // Todos los quests (id y nombre)
+
 router.get("/:id", getDailyQuestsById);           // Quest específico por ID
-router.get("/description/:description", getDescription); // Filtrar por descripción
+
 
 router.post("/", createDailyQuest);
 
