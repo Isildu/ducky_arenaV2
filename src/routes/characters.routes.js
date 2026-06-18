7a38d1e82d4abbe434d6b78fded1d1ab09844df0
Oriol Canellas <oriolcanellas3@gmail.com>
2026-06-18 09:31:28 +0200

    dsf

--- src/routes/characters.routes.js
+++ src/routes/characters.routes.js
@@ -8,9 +8,10 @@ const {
  createCharacter
 } = require("../controllers/characters.controller.js");
 
-router.get("/", getName);             // Devuelve id y nombre de todos
-router.get("/role", getRole);         // Filtra por rol usando ?type=VALOR
 router.get("/all", getCharacters);     // Devuelve todos los personajes completos
+router.get("/role", getRole);         // Filtra por rol usando ?type=VALOR
+router.get("/", getName);             // Devuelve id y nombre de todos
+
 router.get("/:id", getCharactersById); // Devuelve el personaje completo por su ID
 
 router.post("/", createCharacter);
